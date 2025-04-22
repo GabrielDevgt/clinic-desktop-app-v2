b6bfbaa10139dfa40fe6c25769de142ee28ae288
GabrielDevgt <roldangabriel002@gmail.com>
2025-04-22 15:35:52 -0600

    Estructura de Navbar con todos los componentes que la asisten, unicamente CSS y Ts agregado, seguido se inicia pacientes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PacientesComponent } from './pages/pacientes/pacientes.component';
+import { DoctoresComponent } from './pages/doctores/doctores.component';
+import { ConsultasComponent } from './pages/consultas/consultas.component';
+import { CitasComponent } from './pages/citas/citas.component';
+import { TratamientosComponent } from './pages/tratamientos/tratamientos.component';
+import { ExamenesComponent } from './pages/examenes/examenes.component';
+import { BusquedaComponent } from './pages/busqueda/busqueda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'pacientes', component: PacientesComponent },
+  { path: 'doctores', component: DoctoresComponent },
+  { path: 'consultas', component: ConsultasComponent },
+  { path: 'busqueda', component: BusquedaComponent},
+  { path: 'citas', component: CitasComponent },
+  { path: 'tratamientos', component: TratamientosComponent },
+  { path: 'examenes', component: ExamenesComponent },
+];
